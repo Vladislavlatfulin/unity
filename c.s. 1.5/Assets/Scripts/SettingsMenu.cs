02794c9c96c79df9dc856d9f79d2ce53c3512b15
Vlad Latfulin <81932668+Vladislavlatfulin@users.noreply.github.com>
2021-12-29 15:07:55 +0300

    gg

--- c.s. 1.5/Assets/Scripts/SettingsMenu.cs
+++ c.s. 1.5/Assets/Scripts/SettingsMenu.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SettingsMenu : MonoBehaviour
 {
+
+    [SerializeField] private Slider slider;
+    private void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("Speed", 1);
+    }
+
     public void Close ()
     {
         this.gameObject.SetActive(false);
@@ -12,5 +21,16 @@ public class SettingsMenu : MonoBehaviour
     public void Open ()
     {
         gameObject.SetActive(true);
+    }
+
+    public void OnSubmitName(string name)
+    {
+        Debug.Log("Name: " + name);
+    }
+
+    public void OnSpeedValue(float speed)
+    {
+        Debug.Log("Speed: " + speed);
+        PlayerPrefs.SetFloat("Speed", speed);
     }
 }
