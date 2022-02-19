a523eaf91b6c25d724f4a41059760966e87566a5
Vlad Latfulin <81932668+Vladislavlatfulin@users.noreply.github.com>
2022-02-19 13:59:35 +0300

    Revert "add platforms"
    This reverts commit 05008ba737d57f4f95f83e961a21de79f5c15f80.

--- platformer/Assets/Scenes/Scripts/PlayerController.cs
+++ platformer/Assets/Scenes/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+// распознавание поверхностей 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 250f;
@@ -10,64 +11,21 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D _body;
     private Animator _animator;
-    private CapsuleCollider2D _capsule;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
         _body = GetComponent<Rigidbody2D>();
-        _capsule = GetComponent<CapsuleCollider2D>();
     }
 
     private void Update()
     {
         float deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-       
+        _animator.SetFloat("Speed", Mathf.Abs(deltaX));
         Vector2 movement = new Vector2(deltaX, _body.velocity.y);
         _body.velocity = movement;
 
-        Vector3 max = _capsule.bounds.max;
-        Vector3 min = _capsule.bounds.min;
-        Vector3 corner1 = new Vector3(max.x, min.y - 0.1f);
-        Vector3 corner2 = new Vector3(min.x, min.y - 0.2f);
-        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
-
-
-        bool grounded = false;
-        MovingPlatform platform = null;
-        if (hit != null)
-        {
-            grounded = true;
-            platform = hit.GetComponent<MovingPlatform>();
-        }
-
-        if (platform != null)
-        {
-            transform.parent = platform.transform;
-        }
-        else
-        {
-            transform.parent = null;
-        }
-
-        _animator.SetFloat("Speed", Mathf.Abs(deltaX));
-
-        Vector3 pScale = Vector3.one;
-
-        if (platform != null)
-        {
-            pScale = platform.transform.localScale;
-        }
-
-        if (deltaX != 0)
-        {
-            transform.localScale = new Vector3(Mathf.Sign(deltaX) / pScale.x, 1/pScale.y, 1);
-        }
-
-
-
-        _body.gravityScale = grounded && deltaX == 0 ? 0 : 1;
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             _body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
